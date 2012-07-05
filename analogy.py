af210949e37094e92d8c3870ed39ba73bf54903f
Matt Stanton <matt@mattstanton.com>
2012-07-05 12:47:54 -0700

    PCA features.

--- analogy.py
+++ analogy.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
+import os
 import wave
 
 import matplotlib.pyplot
 import numpy
+import scipy.spatial
 
 SAMPLE_RATE = 44100
 SAMPLE_BYTES = 2
 CHANNELS = 1
+
+DATA_DIR = os.path.expanduser('~/Dropbox/Research/analogies/')
 
 def ReadWav(filename):
   wav = None
@@ -51,6 +55,33 @@ def WriteWav(filename, data, channels=1, sample_rate=44100):
     if wav:
       wav.close()
 
-pxx, freqs, bins, im = matplotlib.pyplot.specgram(ReadWav('test.wav'), Fs=SAMPLE_RATE, NFFT=512, noverlap=256)
-print pxx.shape
-matplotlib.pyplot.show()
+def STFT(data):
+  pxx, freqs, bins, im = matplotlib.pyplot.specgram(data, Fs=SAMPLE_RATE, NFFT=256, noverlap=128)
+  #pxx = numpy.log(pxx)
+  return pxx
+
+def MakeFeatures(stfts):
+  data = numpy.hstack(stfts)
+  mean = numpy.mean(data, axis=1)
+  u, s, v = numpy.linalg.svd(data, full_matrices=False)
+  basis = u[:,:2]
+  projected = []
+  for stft in stfts:
+    stft -= mean[:, numpy.newaxis]
+    stft_proj = numpy.dot(basis.T, stft)
+    print numpy.linalg.norm(numpy.dot(basis, stft_proj)) / numpy.linalg.norm(stft)
+    projected.append(stft_proj)
+  return projected
+
+a = ReadWav(DATA_DIR + 'a.wav')
+ap = ReadWav(DATA_DIR + 'ap.wav')
+b = ReadWav(DATA_DIR + 'b.wav')
+
+a_fft = STFT(a)
+ap_fft = STFT(ap)
+b_fft = STFT(b)
+
+features = MakeFeatures([a_fft, ap_fft, b_fft])
+
+#ann = scipy.spatial.cKDTree()
+
