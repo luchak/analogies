efcb1ff5a03ed9fd83e38d6d95b80e25ca0440e4
Matt Stanton <luchak@gmail.com>
2012-07-05 22:56:48 -0700

    Approximate matches. No coherence matches yet.

--- analogy.py
+++ analogy.py
@@ -6,6 +6,7 @@ import wave
 import matplotlib.pyplot
 import numpy
 import scipy.spatial
+import scipy.ndimage
 
 SAMPLE_RATE = 44100
 SAMPLE_BYTES = 2
@@ -56,15 +57,15 @@ def WriteWav(filename, data, channels=1, sample_rate=44100):
       wav.close()
 
 def STFT(data):
-  pxx, freqs, bins, im = matplotlib.pyplot.specgram(data, Fs=SAMPLE_RATE, NFFT=256, noverlap=128)
-  #pxx = numpy.log(pxx)
+  pxx, freqs, bins, im = matplotlib.pyplot.specgram(data, Fs=SAMPLE_RATE, NFFT=512, noverlap=256)
+  pxx = numpy.log(pxx)
   return pxx
 
 def MakeFeatures(stfts):
   data = numpy.hstack(stfts)
   mean = numpy.mean(data, axis=1)
   u, s, v = numpy.linalg.svd(data, full_matrices=False)
-  basis = u[:,:2]
+  basis = u[:,:10]
   projected = []
   for stft in stfts:
     stft -= mean[:, numpy.newaxis]
@@ -72,6 +73,23 @@ def MakeFeatures(stfts):
     print numpy.linalg.norm(numpy.dot(basis, stft_proj)) / numpy.linalg.norm(stft)
     projected.append(stft_proj)
   return projected
+
+def BinomialCoefs(size):
+  coefs = numpy.array([1.0, 1.0])
+  while len(coefs) < size:
+    coefs = numpy.convolve(coefs, numpy.array([1.0, 1.0]))
+  return coefs / coefs.sum()
+
+def MakePyramid(data):
+  pyramid = [data.copy()]
+  coefs = BinomialCoefs(5)
+  while data.shape[1] > len(coefs):
+    data = scipy.ndimage.filters.convolve1d(data, coefs, axis=1)
+    data = data[:,::2]
+    pyramid.append(data.copy())
+  pyramid.reverse()
+  return pyramid
+  
 
 a = ReadWav(DATA_DIR + 'a.wav')
 ap = ReadWav(DATA_DIR + 'ap.wav')
@@ -82,6 +100,68 @@ ap_fft = STFT(ap)
 b_fft = STFT(b)
 
 features = MakeFeatures([a_fft, ap_fft, b_fft])
+pyramids = [MakePyramid(f) for f in features]
+a_pyr = pyramids[0]
+ap_pyr = pyramids[1]
+b_pyr = pyramids[2]
 
-#ann = scipy.spatial.cKDTree()
+result_pyramid = []
 
+c3 = BinomialCoefs(3)
+c5 = BinomialCoefs(5)
+
+def CurrentLevelFeatureVector(a, ap, center, behind, ahead):
+  vector = []
+  for i in range(-behind, ahead + 1):
+    vector.append(c5[i+2] * a[:,center+i])
+    if (i < 0):
+      vector.append(c5[i+2] * ap[:,center+i])
+  return numpy.array(vector).flatten()
+
+def UpperLevelFeatureVector(a, ap, center, behind, ahead):
+  vector = []
+  for i in range(-behind, ahead + 1):
+    vector.append(c3[i+1] * a[:,center+i])
+    vector.append(c3[i+1] * ap[:,center+i])
+  return 1.6*numpy.array(vector).flatten()
+
+index_pyr = []
+for level in range(len(b_pyr)):
+  indices = []
+  for i in range(b_pyr[level].shape[1]):
+    best = -1
+    best_dist = 1e50
+
+    look_behind = min(i, 2)
+    look_ahead = min(b_pyr[level].shape[1] - i - 1, 2)
+    if level > 0:
+      i_upper = i/2
+      look_behind_upper = min(i/2, 1)
+      look_ahead_upper = min(b_pyr[level-1].shape[1] - i_upper - 1, 1)
+
+    search_vec_current = CurrentLevelFeatureVector(b_pyr[level], b_pyr[level][:,indices], i, look_behind, look_ahead)
+    if level > 0:
+      search_vec_upper = UpperLevelFeatureVector(b_pyr[level-1], b_pyr[level-1][:,index_pyr[level-1]], i_upper, look_behind_upper, look_ahead_upper)
+
+    for j in range(look_behind, a_pyr[level].shape[1] - look_ahead):
+      diff_current = search_vec_current - CurrentLevelFeatureVector(a_pyr[level], ap_pyr[level], j, look_behind, look_ahead)
+      dist = numpy.dot(diff_current, diff_current)
+      if level > 0:
+        j_upper = j/2
+        diff_upper = search_vec_upper - UpperLevelFeatureVector(a_pyr[level-1], ap_pyr[level-1], j/2, look_behind_upper, look_ahead_upper)
+        dist += numpy.dot(diff_upper, diff_upper)
+      if dist < best_dist:
+        best_dist = dist
+        best = j
+
+    indices.append(best)
+  print indices
+  index_pyr.append(indices)
+
+triangle = numpy.concatenate((numpy.linspace(0.0, 1.0, 256), numpy.linspace(1.0, 0.0, 256)))
+result = numpy.zeros((256 * (1 + b_fft.shape[1])))
+final_idx = index_pyr[-1]
+for i, idx, in enumerate(final_idx):
+  result[i*256:i*256 + 512] += triangle * ap[indices[i]*256:indices[i]*256 + 512]
+
+WriteWav('bp.wav', result)
